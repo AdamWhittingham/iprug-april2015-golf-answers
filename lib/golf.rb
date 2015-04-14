5888c8d6e7084d720665cfa301326509b3f5ff66
Adam Whittingham <adam.whittingham@gmail.com>
2015-04-14 20:52:46 +0100

    Hole 2 complte

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,8 +1,19 @@
 class Golf
-  def hole_1
+  def hole_1 l
+    n=0
+    a=l[/[A-Z]/]?'A':'a'
+    (a..l)
+      .to_a
+      .map{|a|a*(n+=1)}
+      .join"\n"
   end
 
-  def hole_2
+  def hole_2 s
+    s.split
+     .each_with_object({}){|w,h|h[w]=[w.gsub(/[aeiou]/i,'').length,w.gsub(/[^(aeiou)]/i,'').length]}
+     .sort_by{|k,(v,w)|[v,w,k.upcase]}
+     .flat_map{|a|a.first}
+     .join' '
   end
 
   def hole_3
